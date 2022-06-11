b115ae03500ac4a57415e119f7a75336e2aab828
Sachika <souhila_aouad@outlook.fr>
2022-06-12 00:08:12 +0100

    second commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -139,7 +139,7 @@ useEffect(
         const nextSlide = () => {
             setCurrent(current => (current === length-1 ? 0 : current + 1));
         };
-        timeout.current = setTimeout(nextSlide, 3000);
+        timeout.current = setTimeout(nextSlide, 1000);
         return function(){
             if( timeout.current){
                 clearTimeout(timeout.current);
--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -6,14 +6,6 @@ import ImageFive from '../images/pic5.jpg'
 import Imagesix from '../images/pic6.jpg'
 import Imageseven from '../images/pic7.jpg'
 
-import Imageeight from '../images/pic8.jpg'
-import Imagenine from '../images/pic9.jpg'
-import Imageten from '../images/pic10.jpg'
-import Imageeleven from '../images/pic11.jpg'
-import Imagetwelve from '../images/pic12.jpg'
-
-
-
 
 export const SliderData = [
     {
@@ -71,45 +63,5 @@ export const SliderData = [
         label: 'View Flower',
         image: Imageseven,
         alt: 'Flower'
-    },
-    {
-        title: 'flowers from the heart',
-        price: 'as long as there are flowers to bloom.life is not allowed to be gloomy',
-        path: '/home',
-        label: 'View Flower',
-        image: Imageeight,
-        alt: 'Flower'
-    },
-    {
-        title: 'flowers from the heart',
-        price: 'as long as there are flowers to bloom.life is not allowed to be gloomy',
-        path: '/home',
-        label: 'View Flower',
-        image: Imagenine,
-        alt: 'Flower'
-    },
-    {
-        title: 'flowers from the heart',
-        price: 'as long as there are flowers to bloom.life is not allowed to be gloomy',
-        path: '/home',
-        label: 'View Flower',
-        image: Imageten,
-        alt: 'Flower'
-    },
-    {
-        title: 'flowers from the heart',
-        price: 'as long as there are flowers to bloom.life is not allowed to be gloomy',
-        path: '/home',
-        label: 'View Flower',
-        image: Imageeleven,
-        alt: 'Flower'
-    },
-    {
-        title: 'flowers from the heart',
-        price: 'as long as there are flowers to bloom.life is not allowed to be gloomy',
-        path: '/home',
-        label: 'View Flower',
-        image: Imagetwelve,
-        alt: 'Flower'
-    } 
+    }
 ];
